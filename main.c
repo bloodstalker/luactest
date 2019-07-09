a6051164218836ee0e254e67c04b25d534c49d17
bloodstalker <thabogre@gmail.com>
2019-07-09 22:50:39 +0430

    updated the show hints and tab completions

--- main.c
+++ main.c
@@ -20,16 +20,6 @@
 /**************************************************************************************************/
 lua_State* ls;
 
-void get_lua_global_table(lua_State* _ls) {
-  lua_pushglobaltable(_ls);
-  lua_pushnil(_ls);
-  while(0 != lua_next(_ls, -2)) {
-    printf("%s\n", lua_tostring(_ls, -2));
-    lua_pop(_ls, 1);
-  }
-  lua_pop(_ls, 1);
-}
-
 size_t get_str_len(const char* str) {
   int size_counter = 0;
   while (true) {
@@ -62,6 +52,35 @@ int devi_find_last_word(char const * const str, char* delimiters, int delimiter_
   return 0;
 }
 
+int devi_find_last_word_bi(char const * const str, char* delimiters, int delimiter_size) {
+  size_t str_len = get_str_len(str);
+  bool is_delimiter = false;
+
+  for (int i = 0; i < delimiter_size; ++i) {
+    if (delimiters[i] == str[str_len-1]) return -1;
+  }
+
+  for (int i = str_len -1; i >=0; --i) {
+    for (int j = 0; j < delimiter_size;++j) {
+      if (delimiters[j] == str[i]) {
+        return i + 1;
+      } else {
+        /*intentionally left blank*/
+      }
+    }
+  }
+
+  return 0;
+}
+
+size_t devi_rfind(char const * const str, char const * const substr) {
+  size_t str_len = get_str_len(str);
+  size_t substr_len = get_str_len(substr);
+  for (size_t i = str_len-1; i >=0 ; --i) {
+    if (substr[substr_len-1] != str[i]) {
+      continue;
+    } else {
+      bool matched = true;
 size_t devi_rfind(char const * const str, char const * const substr) {
   size_t str_len = get_str_len(str);
   size_t substr_len = get_str_len(substr);
@@ -107,6 +126,17 @@ size_t devi_lfind(char const * const str, char const * const substr) {
   return -1;
 }
 
+void get_lua_global_table(lua_State* _ls) {
+  lua_pushglobaltable(_ls);
+  lua_pushnil(_ls);
+  while(0 != lua_next(_ls, -2)) {
+    printf("%s\t", lua_tostring(_ls, -2));
+    printf(":\t%d\n", get_str_len(lua_tostring(_ls, -2)));
+    lua_pop(_ls, 1);
+  }
+  lua_pop(_ls, 1);
+}
+
 void shell_completion(const char* buf, linenoiseCompletions* lc) {
   if (NULL != buf) {
     int last_word_index = devi_find_last_word(buf, ID_BREAKERS, NELEMS(ID_BREAKERS));
@@ -124,6 +154,22 @@ void shell_completion(const char* buf, linenoiseCompletions* lc) {
         free(ln_matched);
       }
     }
+
+    lua_pushglobaltable(ls);
+    lua_pushnil(ls);
+    while(0 != lua_next(ls, -2)) {
+      if ( 0 == devi_lfind(lua_tostring(ls, -2), last_word)) {
+        ln_matched = malloc(last_word_index + strlen(lua_tostring(ls, -2)) + 1);
+        ln_matched[last_word_index + strlen(lua_tostring(ls, -2))] = '\0';
+        memcpy(ln_matched, buf, last_word_index);
+        memcpy(&ln_matched[last_word_index], lua_tostring(ls, -2), strlen(lua_tostring(ls, -2)));
+        linenoiseAddCompletion(lc, ln_matched);
+        free(ln_matched);
+      }
+      lua_pop(ls, 1);
+    }
+    lua_pop(ls, 1);
+
     free(last_word);
   }
 }
@@ -148,6 +194,22 @@ char* shell_hint(const char* buf, int* color, int* bold) {
       return ln_matched;
     }
   }
+
+  lua_pushglobaltable(ls);
+  lua_pushnil(ls);
+  while(0 != lua_next(ls, -2)) {
+    size_t match_index = devi_lfind(lua_tostring(ls, -2), last_word);
+    if (0 == match_index) {
+      ln_matched = malloc(strlen(lua_tostring(ls, -2))-strlen(last_word)+1);
+      ln_matched[strlen(lua_tostring(ls, -2)) - strlen(last_word)] = '\0';
+      memcpy(ln_matched, &lua_tostring(ls, -2)[match_index+strlen(last_word)], strlen(lua_tostring(ls, -2)) - strlen(last_word));
+      free(last_word);
+      return ln_matched;
+    }
+    lua_pop(ls, 1);
+  }
+  lua_pop(ls, 1);
+
   free(last_word);
   return NULL;
 }
